032a296b7bf7330d032c53e93ff2312d8f2aad43
Juanse <jsvargasc@unal.edu.co>
2022-03-25 17:47:50 -0500

    Pointers

--- code/pointers.c
+++ code/pointers.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int sumar(int *ap){
+int sumar(int *ap)
+{
     int c;
     c = *ap + 1;
     return c;
 }
 
-int main(){
-    int *ap;
-    ap = malloc(100*sizeof(int));
-    int x = 5;
-    sumar(&x); //dirección de una VARIABLE, no de un apuntador
+int main()
+{
 
-    sumar(ap);
+    int *ap; // * significa reservar una variable para guardar una dirección en memoria. No es la reserva de memoria.
+    ap = malloc(100 * sizeof(int)); // alocar la memoria.
 
-    if (ap==NULL){ //en caso de error ap regresa null
+    if (ap == NULL) // Manejo de error
+    { //en caso de error ap regresa null
         perror("error maloc: ");
         exit(-1); //saca el programa de ejecución
     }
+    
+    int x = 5;
+    //sumar(&x); // &: deberìa usar &, dado que x es una variable, pero sumar me pide un apuntador x*, entonces & me da el apuntador a memoria, &x
 
-    for(int i=0; i< 100; i++){
+    //sumar(ap);
+
+    for(int i = 0; i < 100; i++)
+    {
         // ap[i] = i; //Forma tradicional
-        *(ap+i) = i; //Con apuntadores
+        *(ap + i) = sumar(&x) + i; //Con apuntadores
     }
 
+    for (int i = 0; i < 100; i++)
+    {
+        //printf("%i, ", ap[i]); 
+        printf("%i, ", *(ap + i) ); //*(ap + i) = i + 3; -> ap (es una dirección) sumo i (un entero) entonces avanza 4 bytes. Para eso se hace int* ap, para aclarar
+    }
+    
+    free(ap);
     return 0;
 }
--- code/threads_review.c
+++ code/threads_review.c
@@ -3,11 +3,12 @@
 #include <pthread.h>
  //Imprimir a y b con 8 hilos
 
-void *funcion(void *ap){
+void *funcion(void *ap) {
     struct data *x;
     x = ap;
-    printf("  %i, %i", x->a, x->b);
+    printf("a: %i, b: %i", x->a, x->b);|
 }
+
 
 struct data{
     int a;
@@ -20,15 +21,18 @@ int main(){
     struct data arg[8]; //falta for para llenar datos de lo contrario, SALE MAL.
     void **retval;
 
-    for(int i=0; i<8; i++){
+    for(int i=0; i<8; i++)
+    {
         int r = pthread_create(&thread[i], NULL, funcion, (void*) &arg[i]);
-        if(r!=0){
+        if(r != 0)
+        {
             perror("Salio mal");
             exit(-1);
         }
     }
 
-    for(int i=0; i<8;i++){
+    for(int i=0; i < 8; i++)
+    {
         pthread_join(thread[i], retval);
     }
     return 0;
